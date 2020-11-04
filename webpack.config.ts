1fc23d8c8ed65f610c3b5792f155961b0b8e88af
Tyler Long <tyler4long@gmail.com>
2020-11-03 16:58:27 -0800

    webpack

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+/* eslint-disable node/no-unpublished-import */
+import path from 'path';
+import dotenv from 'dotenv-override-true';
+import {DefinePlugin} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const config = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new DefinePlugin({
+      'process.env': JSON.stringify(dotenv.config().parsed),
+    }),
+    new HtmlWebpackPlugin({
+      title: 'RingCentral Discovery Demo',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
+
+export default [config];
